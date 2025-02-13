26ef8a46529f19810fd9d393efcd739277e3578d
Vinicius Melo <viniciusmelo317@gmail.com>
2025-02-13 15:53:37 -0300

    criado container de repositório, form de pesquisa + estilizações

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,26 @@
+import { FormEvent } from 'react';
+import { Container, Form, SubmitButton } from './styles';
+import { FaGithub, FaPlus } from 'react-icons/fa';
+
 const index = () => {
-  return <div>Home</div>;
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+  };
+
+  return (
+    <Container>
+      <h1>
+        <FaGithub size={25} />
+        Meus Repositórios
+      </h1>
+      <Form onSubmit={handleSubmit}>
+        <input type="text" placeholder="adicionar repositórios" autoFocus />
+        <SubmitButton>
+          <FaPlus color="#fff" size={15} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 };
 
 export default index;
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -4,8 +4,29 @@ const GlobalStyles = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
+    outline: 0;
     box-sizing: border-box;
     font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+
+  html, body, #root {
+    min-height: 100%;
+  }
+
+  body {
+    background-color: #0D2636;
+    font-size: 14px;
+    -webkit-font-smoothing: antialiased !important;
+    padding: 0 2%;
+  }
+
+  body, input, button {
+    color: #222;
+    font-size: 14px;
+  }
+
+  button {
+    cursor: pointer;
   }
 `;
 
